2de790172d502936fe79018488f4204846ee1d71
PrasadLalu <lalumahato020@gmail.com>
2024-09-16 12:43:07 +0530

    Setup HuggingFace with embeddings

---
+++ GenerativeAI/Escape/src/index.ts
@@ -0,0 +1,14 @@
+import { HfInference } from "@huggingface/inference";
+
+const inference = new HfInference(process.env.HF_TOKEN);
+
+async function main() {
+    const output = await inference.featureExtraction({
+      inputs: ["My Cool Embeddings"],
+      model: "BAAI/bge-small-en-v1.5",
+    });
+
+    console.log(output);
+}
+
+main();
