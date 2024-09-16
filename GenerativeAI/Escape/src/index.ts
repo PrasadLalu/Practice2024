969aee9dd8c2a323605eecae600e462c2cd25f53
PrasadLalu <lalumahato020@gmail.com>
2024-09-16 18:36:07 +0530

    Add Hugging Face image model

--- GenerativeAI/Escape/src/index.ts
+++ GenerativeAI/Escape/src/index.ts
@@ -1,3 +1,4 @@
+import { rmSync, writeFile } from "fs";
 import { HfInference } from "@huggingface/inference";
 
 const inference = new HfInference(process.env.HF_TOKEN);
@@ -45,7 +46,23 @@ async function answerQuestion() {
   console.log(result);
 }
 
+async function textToImage() {
+  const result = await inference.textToImage({
+    inputs: "Cat in the hat on the mat",
+    model: "stabilityai/stable-diffusion-2",
+    parameters: {
+      negative_prompt: "blurry",
+    },
+  });
+
+  const buffer = Buffer.from(await result.arrayBuffer());
+  writeFile("image.png", buffer, () => {
+    console.log("image saved");
+  });
+}
+
 // embeddings();
 // translate();
 // translate2();
-answerQuestion();
+// answerQuestion();
+textToImage();
